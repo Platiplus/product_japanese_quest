b9e4a0950d935a925eaef1242fac9c8c4d37d3d6
Victor Rosa <quazarweb@gmail.com>
2023-03-22 19:42:24 -0300

    Solving CHI bug

--- src/utils/regex.util.ts
+++ src/utils/regex.util.ts
@@ -1,5 +1,5 @@
 export const HIRAGANA_KEYBOARD = new RegExp(
-  '(ka|ki|ku|ke|ko|ga|gi|gu|ge|go|sa|shi|su|se|so|za|ji|zu|ze|zo|ta|ti|tsu|te|to|da|dji|dzu|de|do|na|ni|nu|ne|no|ha|hi|fu|he|ho|ba|bi|bu|be|bo|pa|pi|pu|pe|po|ma|mi|mu|me|ya|yu|yo|ra|ri|ru|re|ro|wa|wo|a|i|u|e|o|n)',
+  '(ka|ki|ku|ke|ko|ga|gi|gu|ge|go|sa|shi|su|se|so|za|ji|zu|ze|zo|ta|chi|tsu|te|to|da|dji|dzu|de|do|na|ni|nu|ne|no|ha|hi|fu|he|ho|ba|bi|bu|be|bo|pa|pi|pu|pe|po|ma|mi|mu|me|ya|yu|yo|ra|ri|ru|re|ro|wa|wo|a|i|u|e|o|n)',
   'i',
 )
 export const H_SMALL_KANA_KEYBOARD = new RegExp('(=ya|=yu|=yo|=tsu|=a|=i|=u|=e|=o)', 'i')
@@ -7,7 +7,7 @@ export const H_N_VARIANT_KEYBOARD = new RegExp('(んa|んi|んu|んe|んo)', 'i'
 export const HIRAGANA_PUNCTUATION = new RegExp('(-|,|\\.|"\\s|\\s")', 'i')
 
 export const KATAKANA_KEYBOARD = new RegExp(
-  '(ka|ki|ku|ke|ko|ga|gi|gu|ge|go|sa|shi|su|se|so|za|ji|zu|ze|zo|ta|ti|tsu|te|to|da|dji|dzu|de|do|na|ni|nu|ne|no|ha|hi|fu|he|ho|ba|bi|bu|be|bo|pa|pi|pu|pe|po|ma|mi|mu|me|ya|yu|yo|ra|ri|ru|re|ro|wa|wo|a|i|u|e|o|n)',
+  '(ka|ki|ku|ke|ko|ga|gi|gu|ge|go|sa|shi|su|se|so|za|ji|zu|ze|zo|ta|chi|tsu|te|to|da|dji|dzu|de|do|na|ni|nu|ne|no|ha|hi|fu|he|ho|ba|bi|bu|be|bo|pa|pi|pu|pe|po|ma|mi|mu|me|ya|yu|yo|ra|ri|ru|re|ro|wa|wo|a|i|u|e|o|n)',
   'i',
 )
 export const K_SMALL_KANA_KEYBOARD = new RegExp('(=ya|=yu|=yo|=tsu|=a|=i|=u|=e|=o)', 'i')
